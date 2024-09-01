9bb1d48857952a5e2d8a6d51c31206011fd5ac3e
Zain2006ullah <zain2006ullah@gmail.com>
2024-09-01 10:49:01 +0500

    complete some work in HTML and CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        question: "What is the capital of pakistan?",
+        choices: ("multan", "islamabad", "punjab", "lahore"),
+        answer: 2
+    },
+    {
+        question: "what is the name of your qualification ''?",
+        choices: ("middle", "high", "secondry", ""),
+        answer: 3
+    },
+    {
+        question: "What comes after C ?",
+        choices: ("F", "D", "k", "S"),
+        answer: 2
+    }
+];
